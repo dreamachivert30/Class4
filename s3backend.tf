b6153ed7a3b618e1ac745004a846c89641b08fac
Leela Gravarajapet <Leela.Gravarajapet@viavisolutions.com>
2020-02-27 18:50:16 +0530

    Lock file added

--- s3backend.tf
+++ s3backend.tf
@@ -3,5 +3,8 @@ terraform {
     bucket = "dreamachiversterraform"
     key    = "class4/mytfstatefile.state"
     region = "us-east-1"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
   }
 }
